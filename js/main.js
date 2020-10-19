ec4891e86ab8ae8ae633309a0bef6b432f2a499e
Alfredo Di Forti <adf.palermo@gmail.com>
2020-10-19 14:13:06 +0200

    Inserimento cognome e ordinamento lista

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// chiedi il cognome
+var richiesta = prompt('inserisci il tuo cognome');
+
+//array + aggiunta
+var lista = ['Bianchi', 'Rossi', 'Duzioni', 'Balsano', 'Giorgini'];
+var newlist = lista.push(richiesta);
+console.log(lista.sort());
